0154031487c3c436c833df129259c4c1cc6e8b62
日髙 尚輝 <naoki.h@arsaga.jp>
2020-09-19 16:43:19 +0900

    complete todolist

---
+++ src/js/actions/TodoActions.js
@@ -0,0 +1,37 @@
+import dispatcher from "../dispatcher"
+
+export function createTodo(text) {
+    dispatcher.dispatch({
+        type: "CREATE_TODO",
+        text
+    })
+}
+
+export function deleteTodo(id) {
+    dispatcher.Dispatched({
+        type: "DELETE_TODO",
+        id
+    })
+}
+
+export function reloadTodos() {
+    dispatcher.dispatch({ type: "FETCH_TODOS" })
+
+    setTimeout(() => {
+        dispatcher.dispatch({
+            type: "RECEIVE_TODOS",
+            todos: [
+                {
+                    id: 213464614,
+                    text: "Go Shopping Again",
+                    complete: false
+                },
+                {
+                    id: 335684679,
+                    text: "Sleep At The Yard",
+                    complete: true
+                }
+            ]
+        })
+    }, 1000)
+}
